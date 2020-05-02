662c5f2da2a237b2c9550738f09c60d50c099123
Andrew Gaspar <andrew.gaspar@outlook.com>
2020-05-01 22:21:36 -0600

    Reflect basic Forza telemetry on keyboard

--- forza/src/datagram.rs
+++ forza/src/datagram.rs
@@ -3,53 +3,53 @@ use serde::{Serialize, Serializer};
 #[repr(C)]
 #[derive(Copy, Clone, Serialize)]
 pub struct Vector<T> {
-    x: T,
-    y: T,
-    z: T,
+    pub x: T,
+    pub y: T,
+    pub z: T,
 }
 
 #[repr(C)]
 #[derive(Copy, Clone, Serialize)]
 pub struct Quad<T> {
-    front_left: T,
-    front_right: T,
-    rear_left: T,
-    rear_right: T,
+    pub front_left: T,
+    pub front_right: T,
+    pub rear_left: T,
+    pub rear_right: T,
 }
 
 #[repr(C)]
 #[derive(Copy, Clone, Serialize)]
 pub struct Sled {
-    is_race_on: i32,
-    timestamp_ms: u32,
-    engine_max_rpm: f32,
-    engine_idle_rpm: f32,
-    current_engine_rpm: f32,
-    acceleration: Vector<f32>,
-    velocity: Vector<f32>,
-    angular_velocity: Vector<f32>,
-    yaw: f32,
-    pitch: f32,
-    roll: f32,
-    normalized_suspension_travel: Quad<f32>,
-    tire_split_ratio: Quad<f32>,
-    wheel_rotation_speed: Quad<f32>,
-    wheel_on_rumble_strip: Quad<i32>,
-    wheel_in_puddle_depth: Quad<f32>,
-    surface_rumble: Quad<f32>,
-    tire_slip_angle: Quad<f32>,
-    tire_combined_slip: Quad<f32>,
-    suspension_travel_meters: Quad<f32>,
-    car_ordinal: i32,
-    car_class: i32,
-    car_performance_index: i32,
-    drivetrain_type: i32,
-    num_cylinders: i32,
+    pub is_race_on: i32,
+    pub timestamp_ms: u32,
+    pub engine_max_rpm: f32,
+    pub engine_idle_rpm: f32,
+    pub current_engine_rpm: f32,
+    pub acceleration: Vector<f32>,
+    pub velocity: Vector<f32>,
+    pub angular_velocity: Vector<f32>,
+    pub yaw: f32,
+    pub pitch: f32,
+    pub roll: f32,
+    pub normalized_suspension_travel: Quad<f32>,
+    pub tire_split_ratio: Quad<f32>,
+    pub wheel_rotation_speed: Quad<f32>,
+    pub wheel_on_rumble_strip: Quad<i32>,
+    pub wheel_in_puddle_depth: Quad<f32>,
+    pub surface_rumble: Quad<f32>,
+    pub tire_slip_angle: Quad<f32>,
+    pub tire_combined_slip: Quad<f32>,
+    pub suspension_travel_meters: Quad<f32>,
+    pub car_ordinal: i32,
+    pub car_class: i32,
+    pub car_performance_index: i32,
+    pub drivetrain_type: i32,
+    pub num_cylinders: i32,
 }
 
 #[repr(C)]
 #[derive(Copy, Clone, Serialize)]
-pub struct DashUnpacked {
+struct DashUnpacked {
     position: Vector<f32>,
     // m/s
     speed: f32,
@@ -80,31 +80,31 @@ pub struct DashUnpacked {
 #[repr(C, packed)]
 #[derive(Copy, Clone)]
 pub struct Dash {
-    position: Vector<f32>,
+    pub position: Vector<f32>,
     // m/s
-    speed: f32,
+    pub speed: f32,
     // watts
-    power: f32,
+    pub power: f32,
     // Newton-meter
-    torque: f32,
-    tire_temp: Quad<f32>,
-    boost: f32,
-    fuel: f32,
-    distance_traveled: f32,
-    best_lap: f32,
-    last_lap: f32,
-    current_lap: f32,
-    current_race_time: f32,
-    lap_number: u16,
-    race_position: u8,
-    accel: u8,
-    brake: u8,
-    clutch: u8,
-    hand_brake: u8,
-    gear: u8,
-    steer: i8,
-    normalized_driving_line: i8,
-    normalized_ai_brake_difference: i8,
+    pub torque: f32,
+    pub tire_temp: Quad<f32>,
+    pub boost: f32,
+    pub fuel: f32,
+    pub distance_traveled: f32,
+    pub best_lap: f32,
+    pub last_lap: f32,
+    pub current_lap: f32,
+    pub current_race_time: f32,
+    pub lap_number: u16,
+    pub race_position: u8,
+    pub accel: u8,
+    pub brake: u8,
+    pub clutch: u8,
+    pub hand_brake: u8,
+    pub gear: u8,
+    pub steer: i8,
+    pub normalized_driving_line: i8,
+    pub normalized_ai_brake_difference: i8,
 }
 
 impl Serialize for Dash {
@@ -143,13 +143,13 @@ impl Serialize for Dash {
 #[repr(C)]
 #[derive(Copy, Clone, Serialize)]
 pub struct Horizon4Datagram {
-    sled: Sled,
+    pub sled: Sled,
     // Unsure, but first byte is always decimal 40 (0x28)
-    unknown1: [u8; 4],
+    pub unknown1: [u8; 4],
     // related to depub struction
-    unknown2: f32,
+    pub unknown2: f32,
     // related to depub struction
-    unknown3: f32,
-    dash: Dash,
-    unknown4: i8,
+    pub unknown3: f32,
+    pub dash: Dash,
+    pub unknown4: i8,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
 use std::env;
 use std::mem::size_of;
 
+use chroma::{Key, KeyboardCustomKeyEffectBuilder};
 use futures_util::pin_mut;
+use rgb::RGB8;
 use tokio::stream::StreamExt;
+
+const NUM_KEYS: [chroma::Key; 9] = [
+    Key::Row1,
+    Key::Row2,
+    Key::Row3,
+    Key::Row4,
+    Key::Row5,
+    Key::Row6,
+    Key::Row7,
+    Key::Row8,
+    Key::Row9,
+];
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -10,13 +24,51 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     assert_eq!(79, size_of::<forza::Dash>());
     assert_eq!(324, size_of::<forza::Horizon4Datagram>());
 
-    let server_ip = env::args().skip(1).next().unwrap();
+    let server_ip = env::args().skip(1).next().unwrap_or("0.0.0.0".to_string());
     let server_addr = server_ip + ":8000";
 
     let stream = forza::horizon4(server_addr).await?;
     pin_mut!(stream);
     while let Some(Ok(datagram)) = stream.next().await {
-        println!("{}", serde_json::to_string(&datagram).unwrap());
+        let mut builder = KeyboardCustomKeyEffectBuilder::new();
+        if datagram.sled.is_race_on > 0 {
+            builder.set_key(
+                Key::Space,
+                RGB8 {
+                    r: 0xff,
+                    g: 0xff,
+                    b: 0xff,
+                },
+            );
+        }
+
+        let position = datagram.dash.race_position;
+        if position > 0 && position < 10 {
+            let key = NUM_KEYS[position as usize - 1];
+            let color = if position >= 7 {
+                RGB8 {
+                    r: 0xff,
+                    g: 0,
+                    b: 0,
+                }
+            } else if position >= 4 {
+                RGB8 {
+                    r: 0,
+                    g: 0,
+                    b: 0xff,
+                }
+            } else {
+                RGB8 {
+                    r: 0,
+                    g: 0xff,
+                    b: 0,
+                }
+            };
+
+            builder.set_key(key, color);
+        }
+
+        builder.build()?.set()?;
     }
 
     Ok(())
